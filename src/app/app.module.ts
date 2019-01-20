78d4b4a1a55838b8e1cc8275af2551f000447df8
Princeton Evans <princetonevans@yahoo.com>
2019-01-19 21:15:27 -0600

    installed leaflet-draw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { LeafletDrawModule } from '@asymmetrik/ngx-leaflet-draw';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    LeafletModule.forRoot()
+    FormsModule,
+    LeafletModule.forRoot(),
+    LeafletDrawModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
